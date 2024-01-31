592ccc16e0e212079ea5b1be622955bd60464d89
dean131 <deanabdillah@gmail.com>
2024-01-31 07:02:28 +0700

    Update myadmin/urls.py and base/templates/base/home.html

--- myadmin/views.py
+++ myadmin/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
-
-def index(request):
-    return render(request, 'myadmin/index.html')
+from base.models import KarangTaruna
 
 def login(request):
     return render(request, 'myadmin/login.html')
+
+def beranda_jumbotron(request):
+    if request.method == 'POST':
+        karangtaruna = KarangTaruna.objects.first()
+        nama = request.POST.get('nama')
+        deskripsi_halaman_utama = request.POST.get('deskripsi_halaman_utama')
+        foto_halaman_utama = request.FILES.get('foto_halaman_utama')
+
+        if nama:
+            karangtaruna.nama = nama
+        if deskripsi_halaman_utama:
+            karangtaruna.deskripsi_halaman_utama = deskripsi_halaman_utama
+        if foto_halaman_utama:
+            karangtaruna.foto_halaman_utama = foto_halaman_utama
+        karangtaruna.save()
+
+    karangtaruna = KarangTaruna.objects.first()
+    context = {
+        'karangtaruna': karangtaruna
+    }
+    return render(request, 'myadmin/beranda_jumbotron.html', context)
